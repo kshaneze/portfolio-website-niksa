eccbefd9d6e2389b95371847398afe510351d55f
niksa <niksaopfor@gmail.com>
2022-02-22 23:55:11 +0100

    Working on my protfolio 4

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 'use strict';
+const btnLinkAboutMe = document.querySelector('.btn-link1');
+const aboutMe = document.querySelector('#about__me-link');
+
+aboutMe.scrollIntoView({ behavior: 'smooth' });
